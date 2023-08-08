bbf8aa863c44825c9f9d1c7169f4a5dd60d06254
Roberto <bobo@di.unipmn.it>
2023-08-08 15:17:10 +0200

    Fixed release requirements

--- version.php
+++ version.php
@@ -26,7 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2023061400;
-$plugin->requires = 2018110800;
+$plugin->requires = 2022112800;
+$plugin->supported = [401];
 $plugin->component = 'quiz_concorsi';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0';
